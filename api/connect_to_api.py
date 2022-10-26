46f64f1700cc019b78a4c42bcfc36d84eae0257f
JayCax <johnfcaccamo@gmail.com>
2022-10-26 11:38:26 -0700

    Updated api to reflect Bitmex paper trading platform - placed in a function, renamed file

--- api/connect_to_api.py
+++ api/connect_to_api.py
@@ -1,15 +1,19 @@
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 import pandas as pd
 import mplfinance as mpf
+import bitmex
+import json
+
+# testing with bitmex paper trading
+api_key = "yc9cECO9UjxiUYgEGNP8q7Yb"
+secret = "GTLDBMMXaPtM1-7g7ZQFl96Yqwqd5J1N_8CrPIbyRsgEJ2lg"
+base_url = "https://testnet.bitmex.com/"
 
 def render_API_data():
     """Following are Binance US API keys - not compatible for paper trading in US jurisdiction"""
     # api_key = "BTHSwDSUMGu8fK2PLTN7UtoBGAc2BMXgTLfAO3bwecfMSE5rhbnsHyYna2VzfT9z"
     # secret = "6oza52BTBzRO9CizH5opYewZudWElkzcFUl8BZ6jT3k5YBR6KK3pGCxDIry6czF7"
 
-    # testing with bitmex paper trading
-    api_key = "yc9cECO9UjxiUYgEGNP8q7Yb"
-    secret = "GTLDBMMXaPtM1-7g7ZQFl96Yqwqd5J1N_8CrPIbyRsgEJ2lg"
 
     # install at this step: pip install python-binance pandas mplfinance
 
@@ -65,5 +69,28 @@ def render_API_data():
         mav=(10,20,30))
 
 
+def execute_sample_trade_bitmex():
+    client = bitmex.bitmex(test=True, api_key=api_key, api_secret=secret)
+
+    result = client.Quote.Quote_get(symbol="XBTUSD", reverse=True, count=1).result()
+    print(result[0][0]['bidPrice'])
+
+    print(client.Instrument.Instrument_get(filter=json.dumps({'symbol': 'XBTJPY'})).result())
+
+    symbol = 'XBTUSDT'
+    ordType = 'Market'
+    orderQty_Buy = "100"  # Positive value to long
+    orderQty_Sell = "-100"  # Negative value to short
+    client.Order.Order_new(symbol=symbol, ordType=ordType, orderQty=orderQty_Buy).result()  # Long
+    client.Order.Order_new(symbol=symbol, ordType=ordType, orderQty=orderQty_Sell).result()  # Short
+
+    #client.Order.Order_new(symbol, orderQty=10, price=12345.0).result()
+
+
+def execute_sample_trade_binance():
+    #client.binance
+    pass
+
 if __name__ == "__main__":
-    render_API_data()
+    #render_API_data()
+    execute_sample_trade_bitmex()
