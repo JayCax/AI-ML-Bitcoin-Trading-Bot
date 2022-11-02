131fd7d3e76b9904849e3d66279ac12c5c5e773c
JayCax <johnfcaccamo@gmail.com>
2022-11-02 00:26:14 -0700

    Added basis for bot's trading function

--- api/connect_to_api.py
+++ api/connect_to_api.py
@@ -1,8 +1,11 @@
+# binance, ccxt and bitmex need separate pip install
+
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 import pandas as pd
 import mplfinance as mpf
 import bitmex
 import json
+import ccxt
 
 """
 Following are Binance US API keys - not compatible for paper trading in US jurisdiction
@@ -17,7 +20,8 @@ bitmex_api_key = "yc9cECO9UjxiUYgEGNP8q7Yb"
 bitmex_secret_key = "GTLDBMMXaPtM1-7g7ZQFl96Yqwqd5J1N_8CrPIbyRsgEJ2lg"
 base_url = "https://testnet.bitmex.com/"
 
-# kept separate from Class for now 
+
+# kept separate from Class for now
 def render_API_data():
     # install at this step: pip install python-binance pandas mplfinance
 
@@ -96,8 +100,14 @@ def render_API_data():
     return None
 
 
+def execute_sample_trade_binance():
+    # client.binance
+    pass
+
+
 class BitmexClient:
     def __init__(self, public_key: str, secret_key: str, testnet: bool):
+
         if testnet:
             self._base_url = "https://testnet.bitmex.com"
             self._wss_url = "wss://testnet.bitmex.com/realtime"
@@ -110,29 +120,30 @@ class BitmexClient:
 
         self.client = bitmex.bitmex(test=True, api_key=bitmex_api_key, api_secret=bitmex_secret_key)
 
+        self.bm = ccxt.bitmex({'apiKey': public_key,
+                               'secret': secret_key})
+
+        if 'test' in self.bm.urls:
+            self.bm.urls['api'] = self.bm.urls['test']
+            self.bm.urls['api'] = self.bm.urls['test']
+
         print("Bitmex Client successfully initialized")
 
-    def execute_sample_trade_bitmex(self):
+    """
+    Sample API calls
+    """
 
-        # publish user permissions / access
-        print(self.client.Position.Position_get(filter=json.dumps({'symbol': 'XBTUSD'})).result())
+    def execute_bitmex_api_functions(self):
+        # publish specific api user permissions / access
+        for k, v in self.client.Position.Position_get(filter=json.dumps({'symbol': 'XBTUSD'})).result()[0][0].items():
+            print(str(k) + "   " + str(v))
 
-        # get specific BTC data
+        # get specific BTC data - current bid price
         result = self.client.Quote.Quote_get(symbol="XBTUSD", reverse=True, count=1).result()
         print(result[0][0]['bidPrice'])
 
         # # get the btc data, exchange stats for the pair
         # print(client.Instrument.Instrument_get(filter=json.dumps({'symbol': 'XBTUSD'})).result())
-
-        params = {
-            "symbol": "XBTUSD",
-            "side": "BUY",
-            "type": "LIMIT",
-            "timeInForce": "GTC",
-            "quantity": 0.002,
-            "price": 18000,
-        }
-
 
         # # limit buy
         # self.client.Order.Order_new(symbol='XBTUSD', orderQty=100, price=18000).result()
@@ -140,16 +151,37 @@ class BitmexClient:
         # # spot buy
         # self.client.Order.Order_new(symbol='XBTUSD', orderQty=100).result()
 
-        # limit sell
-        self.client.Order.Order_new(symbol='XBTUSD', orderQty=-100, price=25000).result()
+        # # limit sell
+        # self.client.Order.Order_new(symbol='XBTUSD', orderQty=-100, price=25000).result()
 
+        # # spot sell
+        # self.client.Order.Order_new(symbol='XBTUSD', orderQty=-100).result()
 
-def execute_sample_trade_binance():
-    # client.binance
-    pass
+        # # cancel open orders
+        # self.client.Order.Order_cancelAll().result()
+
+        # publish all historical orders
+        for i in self.client.Order.Order_getOrders(symbol="XBTUSD").result()[0]:
+            print(i)
+
+        return None
+
+    """
+    Specific bot XBT / USDT trading function
+    """
+
+    def execute_bot_trade(self):
+        # # may use cctx to execute trades
+        # print(self.bm.fetch_trades('XBTUSD'))
+
+        # # UNCOMMENT THIS TO EXECUTE TRADE - TO BE CONNECTED TO THE BOT
+        # # spot buy with USDT 1000000 approx ~15 USDT token -arbitrary?
+        # self.client.Order.Order_new(symbol='XBTUSDT', orderQty=1000000).result()
+        pass
 
 
 if __name__ == "__main__":
     # render_API_data()
     # execute_sample_trade_bitmex()
-    test = BitmexClient(True, bitmex_api_key, bitmex_secret_key).execute_sample_trade_bitmex()
+    bitmex_client = BitmexClient(bitmex_api_key, bitmex_secret_key, True)
+    bitmex_client.execute_bitmex_api_functions()
